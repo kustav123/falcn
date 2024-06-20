7a211f898f8ed50722274089ee15bdb3701c08cd
kustav123 <47721564+kustav123@users.noreply.github.com>
2024-06-20 18:38:13 +0530

    dashboard first

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     function index() {
-        return view('admin.dashboard');
+        $totalclientCount = Client::count(); // Get the count of users
+        $clientCount = Client::where('status', 1)->count(); // Get the count of users
+        return view('admin.dashboard', compact('clientCount', 'totalclientCount')); // Pas
     }
 }
